ec0197b440ee2e7ee7669ac12d2d045cb7f06a47
BhavyaKrishna <bhavyakrishna24@gmail.com>
2020-03-01 11:55:46 +0530

    vivado source files for writing control data

---
+++ project_readwrite/project_readwrite.srcs/sim_1/new/write_test.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.02.2020 20:54:58
+// Design Name: 
+// Module Name: write_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module write_test ;
+
+integer i;
+integer file_id;
+reg x1;
+reg x2;
+wire y;
+
+
+AND_2 andgate(y,x1,x2);
+
+
+initial
+    begin
+    $display("checkpoint 1");
+    #10
+   // file_id = $fopen("writtenfile.dat","w+");
+    #10
+    /*for(i=0;i<10;i=i+1)
+    begin
+    $fwrite(file_id,"%02h",i);
+    end*/
+    
+
+ 
+   
+   x1=0;
+   x2=0;
+   #200
+   file_id = $fopen("writtenfile.txt","w+");
+   $fwrite(file_id,"%b",y);
+   $fclose(file_id);
+   
+   
+   x1=1;
+   x2=0;
+   #200
+   //$fwrite(file_id,"%b",y);
+   file_id = $fopen("writtenfile.txt","w+");
+   $fwrite(file_id,"%b",y);
+   $fclose(file_id);
+   
+   
+   x1=0;
+   x2=1;
+   #200
+  // $fwrite(file_id,"%b",y);
+   file_id = $fopen("writtenfile.txt","w+");
+   $fwrite(file_id,"%b",y);
+   $fclose(file_id);   
+   
+   
+   x1=1;
+   x2=1;
+   #200
+ //  $fwrite(file_id,"%b",y);
+    file_id = $fopen("writtenfile.txt","w+");
+   $fwrite(file_id,"%b",y);
+   $fclose(file_id);
+
+       
+end
+
+initial 
+$monitor ($time, " Output y = %d" , y) ;
+
+
+
+endmodule
---
+++ project_readwrite/project_readwrite.srcs/sources_1/new/AND_2.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.02.2020 23:17:57
+// Design Name: 
+// Module Name: AND_2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AND_2(
+    output y,
+    input x1,
+    input x2
+    );
+    
+ and a1(y,x1,x2);
+endmodule
