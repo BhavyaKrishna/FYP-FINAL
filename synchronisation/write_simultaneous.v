85681bf4eb797f64454e6182ad3269ef4ed4e817
BhavyaKrishna <bhavyakrishna24@gmail.com>
2020-04-25 19:47:58 +0530

    synchronisation done

--- synchronisation/write_simultaneous.v
+++ synchronisation/write_simultaneous.v
@@ -1,6 +1,6 @@
 module write_test ;
 
-integer i;
+integer i,code;
 integer file_id;
 reg x1;
 reg x2;
@@ -8,61 +8,66 @@ wire y;
 integer j = 1;
 integer char1, char2, char3;
 //reg vx1 =, vy1 = 0.01d;
-real vx1 = 0, vy1 = 0.01;
-real vx2 = 0.03, vy2 = 0.04;
-real vx3 = 0.06, vy3 = 0.08;
+real vx1 = 0, vy1 = 0.001;
+real vx2 = 0.003, vy2 = 0.004;
+real vx3 = 0.006, vy3 = 0.008;
 always
+  
     begin 
         fork 
            begin
-              #200 file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot1.txt","r");
-              $fscanf(file_id, "%c",char1);
+              #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot1.txt","r");
+              code=$fscanf(file_id, "%c",char1);
               //$display("%c", char1);
               $fclose(file_id);
-              vx1 = vx1 + 0.01;
-              vy1 = vy1 + 0.01;
               if(char1 =="r")
               begin
-              file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot1.txt","w");
-               $fwrite(file_id, "w\n");
-               $fwrite(file_id,vx1,"\n");
-               $fwrite(file_id,vy1,"\n");
-               //$fwrite(file_id, "\n");
-               $display("bot1", $time);
-               $fclose(file_id);
+                  vx1 = -vx1;
+                  vy1 = -vy1 ;
+                  file_id = $fopen("F:/FYP-FINAL/synchronisation/bot1.txt","w");
+                  $fwrite(file_id, "w\n");
+                  $fwrite(file_id,vx1,"\n");
+                  $fwrite(file_id,vy1,"\n");
+                  //$fwrite(file_id, "\n");
+                  $display("bot 1 velocity updated", $time);
+                  $fclose(file_id);
                end
            end
            begin
-              #300 file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot2.txt","r");
-               $fscanf(file_id, "%c",char2);
+               #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot2.txt","r");
+               code=$fscanf(file_id, "%c",char2);
                //$display("%c", char1);
                $fclose(file_id);
-               vx2 = vx2 + 0.01;
-               vy2 = vy2 + 0.01;
                if(char1 =="r")
-               file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot2.txt","w");
-               $fwrite(file_id, "w\n");
-               $fwrite(file_id,vx2,"\n");
-               $fwrite(file_id,vy2,"\n");
-               //$fwrite(file_id, "\n");
-               $display("bot2", $time);
-               $fclose(file_id);
+               begin
+                   vx2 = -vx2;
+                   vy2 = -vy2;
+                   file_id = $fopen("F:/FYP-FINAL/synchronisation/bot2.txt","w");
+                   $fwrite(file_id, "w\n");
+                   $fwrite(file_id,vx2,"\n");
+                   $fwrite(file_id,vy2,"\n");
+                   //$fwrite(file_id, "\n");
+                   $display("bot 2 velocity updated", $time);
+                   $fclose(file_id);
+               end
             end
            begin
-              #100 file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot3.txt","r");
-              $fscanf(file_id, "%c",char3);
+              #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot3.txt","r");
+              code=$fscanf(file_id, "%c",char3);
               //$display("%c", char1);
               $fclose(file_id);
-              vx3 = vx3 + 0.01;
-              vy3 = vy3 + 0.01;
               if(char3 =="r")
-              file_id = $fopen("C:/Users/sujan/Desktop/Suj/studies/Final year project/simultaneous_write/bot3.txt","w");
-              $fwrite(file_id, "w\n");
-              $fwrite(file_id,vx3,"\n");
-              $fwrite(file_id,vy3,"\n");
-              //$fwrite(file_id, "\n");
-              $display("bot3", $time);
-              $fclose(file_id);
+              begin
+                  vx3 = -vx3;
+                  vy3 = -vy3;
+                  file_id = $fopen("F:/FYP-FINAL/synchronisation/bot3.txt","w");
+                  $fwrite(file_id, "w\n");
+                  $fwrite(file_id,vx3,"\n");
+                  $fwrite(file_id,vy3,"\n");
+                  //$fwrite(file_id, "\n");
+                  $display("bot 3 velocity updated", $time);
+                  $fclose(file_id);
+              end
            end 
         join 
         //$display($time);
