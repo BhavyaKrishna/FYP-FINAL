f848f44aed1deeca6929a546c678ed58576b5b47
BhavyaKrishna <bhavyakrishna24@gmail.com>
2020-05-02 16:12:38 +0530

    after closing files

--- synchronisation/write_simultaneous.v
+++ synchronisation/write_simultaneous.v
@@ -13,37 +13,35 @@ integer char1, char2, char3;
 //reg vx1 =, vy1 = 0.01d;
 
 real vx1 = 0, vy1 = 0.001;
-real vx2 = 0.003, vy2 = 0.004;
-real vx3 = 0.006, vy3 = 0.008;
+real vx2 = 0.3, vy2 = 0.4;
+real vx3 = 0.6, vy3 = 0.8;
 
 always @(posedge write_check)
   
     begin 
         vx1=vx1_bin*(1.0/((2**11)*1.0));
         vy1=vy1_bin*(1.0/((2**11)*1.0));
-        vx2=vx2_bin*(1.0/((2**11)*1.0));
+        /*vx2=vx2_bin*(1.0/((2**11)*1.0));
         vy2=vy2_bin*(1.0/((2**11)*1.0));
         vx3=vx3_bin*(1.0/((2**11)*1.0));
-        vy3=vy3_bin*(1.0/((2**11)*1.0));
+        vy3=vy3_bin*(1.0/((2**11)*1.0));*/
         fork 
            begin
-              #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot11.txt","r");
+              #100 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot11.txt","r");
               code=$fscanf(file_id, "%c",char1);
-              //$display("%c", char1);
-              $fclose(file_id);
+              sn$fclose(file_id);
               if(char1 =="r")
               begin
                   file_id = $fopen("F:/FYP-FINAL/synchronisation/bot11.txt","w");
                   $fwrite(file_id, "w\n");
                   $fwrite(file_id,vx1,"\n");
                   $fwrite(file_id,vy1,"\n");
-                  //$fwrite(file_id, "\n");
                   $display("bot 1 velocity updated", $time);
                   $fclose(file_id);
                end
            end
            begin
-               #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot22.txt","r");
+               #200 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot22.txt","r");
                code=$fscanf(file_id, "%c",char2);
                //$display("%c", char1);
                $fclose(file_id);
@@ -59,7 +57,7 @@ always @(posedge write_check)
                end
             end
            begin
-              #300 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot33.txt","r");
+              #400 file_id = $fopen("F:/FYP-FINAL/synchronisation/bot33.txt","r");
               code=$fscanf(file_id, "%c",char3);
               //$display("%c", char1);
               $fclose(file_id);
