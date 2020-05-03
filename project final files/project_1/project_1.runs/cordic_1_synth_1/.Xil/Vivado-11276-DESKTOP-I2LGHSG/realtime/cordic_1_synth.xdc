a69a6f9d102365b978aa719ca7428005810307c9
Sujana-Ramesh <sujanaram98@gmail.com>
2020-05-03 19:07:24 +0530

    Full working mode

--- project final files/project_1/project_1.runs/cordic_1_synth_1/.Xil/Vivado-11276-DESKTOP-I2LGHSG/realtime/cordic_1_synth.xdc
+++
@@ -1,6 +0,0 @@
-set_property SRC_FILE_INFO {cfile:{d:/Repository/FYP-FINAL/project final files/project_1/project_1.srcs/sources_1/ip/cordic_1/cordic_1_ooc.xdc} rfile:../../../../../project_1.srcs/sources_1/ip/cordic_1/cordic_1_ooc.xdc id:1 order:EARLY scoped_inst:U0} [current_design]
-set_property SRC_FILE_INFO {cfile:{D:/Repository/FYP-FINAL/project final files/project_1/project_1.runs/cordic_1_synth_1/dont_touch.xdc} rfile:../../../dont_touch.xdc id:2} [current_design]
-set_property src_info {type:SCOPED_XDC file:1 line:55 export:INPUT save:INPUT read:FILTER_OUT_OF_CONTEXT} [current_design]
-create_clock -period 1000.000 -name aclk [get_ports aclk]
-set_property src_info {type:XDC file:2 line:9 export:INPUT save:INPUT read:READ} [current_design]
-set_property DONT_TOUCH true [get_cells U0]
---
+++ project final files/project_1/project_1.srcs/sources_1/ip/cordic_0/cordic_0_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+-- Date        : Sun May  3 19:06:56 2020
+-- Host        : DESKTOP-I2LGHSG running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {D:/Repository/FYP-FINAL/project final
+--               files/project_1/project_1.srcs/sources_1/ip/cordic_0/cordic_0_stub.vhdl}
+-- Design      : cordic_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7k70tfbv676-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cordic_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_phase_tvalid : in STD_LOGIC;
+    s_axis_phase_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    s_axis_cartesian_tvalid : in STD_LOGIC;
+    s_axis_cartesian_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_dout_tvalid : out STD_LOGIC;
+    m_axis_dout_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end cordic_0;
+
+architecture stub of cordic_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_phase_tvalid,s_axis_phase_tdata[15:0],s_axis_cartesian_tvalid,s_axis_cartesian_tdata[31:0],m_axis_dout_tvalid,m_axis_dout_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "cordic_v6_0_14,Vivado 2018.2";
+begin
+end;
