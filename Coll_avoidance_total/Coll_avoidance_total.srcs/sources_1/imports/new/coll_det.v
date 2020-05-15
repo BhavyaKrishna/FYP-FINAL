45e109d69408712b00658921d4c05d7dea394bf4
D C VIVEK <dc28vivek@gmail.com>
2020-05-15 23:36:57 +0530

    Update coll_det.v

--- Coll_avoidance_total/Coll_avoidance_total.srcs/sources_1/imports/new/coll_det.v
+++ Coll_avoidance_total/Coll_avoidance_total.srcs/sources_1/imports/new/coll_det.v
@@ -1,23 +1,22 @@
 module coll_det(x1, y1, x2, y2, vx1, vy1, vx2, vy2, r2, trial, clock, in_rdy,out_rdy);
 
-input [15:0] x1, y1, x2, y2, vx1, vy1, vx2, vy2, r2;
+input [31:0] x1, y1, x2, y2, vx1, vy1, vx2, vy2, r2;
 output trial;
 input clock,in_rdy;			
-output out_rdy;
+output reg out_rdy;
 
 reg [31:0] X1,Y1,X2,Y2,VX1,VY1,VX2,VY2,R2;    //input registers
 reg trial; //output registers
-reg [31:0] e,f,g,h,i,j,l,n,dot_sq;                               //intermediate registers
-reg [31:0] a,b,c,d,r_sq,vab_sq,k,m,M0A,M0B,M1A,M1B,P0,Q0,P1,Q1,P2,Q2,P3,Q3,P4,Q4,P5,Q5,P6,Q6;
-reg [63:0] C1,C2;
-
-reg output_ready=1'b0;
+reg [31:0] e,f,g,h,i,j;
+reg [63:0] dot_sq,l,m,n;                               //intermediate registers
+reg [31:0] a,b,c,d,r_sq,vab_sq,k,M0A,M0B,M1A,M1B,P0,Q0,P1,Q1,P2,Q2,P3,Q3,P4,Q4,P5,Q5,P6,Q6;
+reg [63:0] C1,C2,P7,Q7;
 
 wire [0:10] carry;
 
 integer count=0;
 wire C0;
-wire[63:0] M0P,M1P;
+wire[63:0] M0P,M1P,S7;
 wire [31:0] S0,S1,S2,S3,S4,S5,S6;
 //instantiations
 
@@ -32,10 +31,8 @@ subtractor_32bit sub0(carry[3],S3,P3,Q3,1'b1);
 subtractor_32bit sub1(carry[4],S4,P4,Q4,1'b1);
 subtractor_32bit sub2(carry[5],S5,P5,Q5,1'b1);
 subtractor_32bit sub3(carry[6],S6,P6,Q6,1'b1);
- 
+subtractor_64bit sub4(carry[7],S7,P7,Q7,1'b1);
 comparator comp1(C0,C1,C2);
-
-assign out_rdy=output_ready;
 
 always @(posedge clock)
  
@@ -97,12 +94,12 @@ always @(posedge clock)
    7:begin       
      dot_sq=M0P;
      l=M1P;    
-     P3<=l; Q3<=dot_sq;
+     P7<=l; Q7<=dot_sq;
      M0A<=vab_sq; M0B<=R2;
     end
 
    8:begin       
-     m=S3;
+     m=S7;
      n=M0P;    
      C1<=m; C2<=n;
     end
@@ -110,10 +107,7 @@ always @(posedge clock)
    9:begin
       trial=C0;  
       count=-1;             //so that after updation it goes to 0
-      output_ready=1'b1;         //out_rdy signal is raised once all outputs are ready 
-      #10;
-      output_ready=1'b0;  //Should be given as a pulse
-      trial =1'bX;        //Same with the trial case :P  
+      out_rdy=1'b0;         //out_rdy signal is raised once all outputs are ready
     end 
      
   endcase
@@ -121,9 +115,3 @@ always @(posedge clock)
  end
      
 endmodule
-
-
-
-
-
-    
