1befa51397863d0fee40a0a7adebb7261e25f233
Sujana-Ramesh <sujanaram98@gmail.com>
2020-05-22 14:08:17 +0530

    VS without PP

---
+++ Coll_avoidance_total/Coll_avoidance_total.srcs/sources_1/imports/new/subtractor_4bit.v
@@ -0,0 +1,38 @@
+module subtractor_4bit(Cout, S, A, B, Cin);
+   output [3:0] S;   // The 4-bit sum/difference.
+   output 	Cout;   // The 1-bit carry/borrow status.
+   input [3:0] 	A;   // The 4-bit augend/minuend.
+   input [3:0] 	B;   // The 4-bit addend/subtrahend.
+   input	Cin;
+   wire 	C0; // The carry out bit of fa0, the carry in bit of fa1.
+   wire 	C1; // The carry out bit of fa1, the carry in bit of fa2.
+   wire 	C2; // The carry out bit of fa2, the carry in bit of fa3.
+   wire 	C3; // The carry out bit of fa2, used to generate final carry/borrrow.
+   
+   wire 	B0; // The xor'd result of B[0] and Op
+   wire 	B1; // The xor'd result of B[1] and Op
+   wire 	B2; // The xor'd result of B[2] and Op
+   wire 	B3; // The xor'd result of B[3] and Op
+
+
+	
+   // Looking at the truth table for xor we see that  
+   // B xor 0 = B, and
+   // B xor 1 = not(B).
+   // So, if Op==1 means we are subtracting, then
+   // adding A and B xor Op alog with setting the first
+   // carry bit to Op, will give us a result of
+   // A+B when Op==0, and A+not(B)+1 when Op==1.
+   // Note that not(B)+1 is the 2's complement of B, so
+   // this gives us subtraction.     
+   xor(B0, B[0], 1'b1);
+   xor(B1, B[1], 1'b1);
+   xor(B2, B[2], 1'b1);
+   xor(B3, B[3], 1'b1);
+   xor(Cout, C3, 1'b1);     // Carry = C3 for addition, Carry = not(C3) for subtraction.
+
+   full_adder fa0(S[0], C0, A[0], B0, Cin);    // Least significant bit.
+   full_adder fa1(S[1], C1, A[1], B1, C0);
+   full_adder fa2(S[2], C2, A[2], B2, C1);
+   full_adder fa3(S[3], C3, A[3], B3, C2);    // Most significant bit.
+endmodule // ripple_carry_adder_subtractor
---
+++ Coll_avoidance_total/Coll_avoidance_total.srcs/sources_1/new/Integrator1.v
@@ -0,0 +1,185 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.04.2020 17:05:10
+// Design Name: 
+// Module Name: Integrator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Integrator1(vx_not_update,vy_not_update,x1,y1,vx1,vy1,x2,y2,vx2,vy2,x3,y3,vx3,vy3,ax,ay,input_rdy,clock,ig_done);
+
+input [15:0] vx_not_update,vy_not_update,x1,y1,vx1,vy1,x2,y2,vx2,vx3,vy2,x3,y3,vy3,ax,ay;
+input input_rdy,clock;
+output ig_done;
+
+reg ig_done_reg=1'b0;
+assign ig_done=ig_done_reg;
+
+wire [15:0] t;
+wire [31:0] R;
+reg [31:0] R_reg=32'd203004;
+
+reg [15:0] vx1_WT,vy1_WT;
+wire [15:0] WT_vx1,WT_vy1;
+ 
+reg input_CD1=1'b0;
+reg input_CD2=1'b0;
+reg input_UM=1'b0;
+reg input_VS=1'b0;
+
+reg output_check_reg=1'b0;
+reg input_check_reg=1'b1;
+
+wire output_in_rdy;
+wire output_written;
+wire input_in_rdy;
+
+reg vs_ip_sel=1'b0;// One from file else from UM
+
+//Update module Variables
+wire [15:0] UM_Vxin,UM_Vyin,xnew,ynew,vxnew,vynew;
+wire UM_in_rdy,UM_out_rdy;
+reg [15:0] Vxnew_UM,Vynew_UM;
+update_module UM (x1, y1, UM_Vxin, UM_Vyin, ax, ay ,t,clock, UM_in_rdy,UM_out_rdy,xnew, ynew, vxnew, vynew);
+
+//Velocity Selector Variables
+wire [15:0] VS_Vxin,VS_Vyin,VS_Vxout,VS_Vyout;
+wire VS_in_rdy,VS_out_rdy;
+reg  [15:0] Vxnew_VS,Vynew_VS;
+Velocity_selector VS (VS_Vxin,VS_Vyin,clock,VS_in_rdy,VS_Vxout,VS_Vyout,VS_out_rdy);
+
+//Update module Variables
+wire [15:0] CD_Vxin,CD_Vyin,CD_xin,CD_yin;
+wire CD1_in_rdy,CD1_out_rdy;
+wire CD2_in_rdy,CD2_out_rdy;
+reg [15:0] Vxnew_CD,Vynew_CD,xnew_CD,ynew_CD;
+wire coll_detect1,coll_detect2;
+coll_det CD1(CD_xin,CD_yin,x2, y2, CD_Vxin, CD_Vyin, vx2, vy2, R, coll_detect1, clock, CD1_in_rdy, CD1_out_rdy);
+coll_det CD2(CD_xin,CD_yin,x3, y3, CD_Vxin, CD_Vyin, vx3, vy3, R, coll_detect2, clock, CD2_in_rdy, CD2_out_rdy);
+
+//Write Test for bot1 
+write_test2 WT(WT_vx1,WT_vy1,output_in_rdy,output_written);
+
+assign R     = R_reg;
+assign t = 16'd1;         //Time should not b e scaled :) for LHS and RHS being okay.
+
+assign CD_xin=xnew_CD;
+assign CD_yin=ynew_CD;
+assign CD_Vxin=Vxnew_CD;
+assign CD_Vyin=Vynew_CD;
+
+assign UM_Vxin=Vxnew_UM;
+assign UM_Vyin=Vynew_UM;
+
+assign VS_Vxin=Vxnew_VS;
+assign VS_Vyin=Vynew_VS;
+
+assign VS_in_rdy    = input_VS;
+assign UM_in_rdy    = input_UM;
+assign CD1_in_rdy    = input_CD1;
+assign CD2_in_rdy    = input_CD2;
+
+assign output_in_rdy =  output_check_reg;
+assign input_in_rdy  =  input_check_reg;
+
+assign WT_vx1        =  vx1_WT;
+assign WT_vy1        =  vy1_WT;
+
+always @(posedge input_rdy)   //Make input_check high for writing into UM from file
+begin
+        input_CD1 = 1'b1;
+        input_CD2 = 1'b1;
+        Vxnew_CD  =  vx1;
+        Vynew_CD  =  vy1;
+        xnew_CD   =  x1;
+        ynew_CD   =  y1;
+        vs_ip_sel = 1'b0; //from file
+
+end
+always @(posedge CD1_out_rdy)
+begin   
+    #10;
+    input_CD1 = 1'b0;
+end
+always @(posedge CD2_out_rdy)
+begin   
+    #10;
+    input_CD2 = 1'b0;
+end
+
+always @(posedge UM_out_rdy)
+begin
+    input_CD1 = 1'b1;
+    input_CD2=  1'b1;
+    xnew_CD  = xnew;
+    ynew_CD  = ynew;
+    Vxnew_CD = vxnew;
+    Vynew_CD = vynew;
+    #10;
+    input_UM = 1'b0;
+end
+
+always @(coll_detect1 | coll_detect2 )   //Load only when collision exist
+begin
+    if((coll_detect1==1)||(coll_detect2 ==1))
+        begin
+        input_VS = 1'b1;
+        case(vs_ip_sel)
+            0:begin
+                Vxnew_VS = vx_not_update;
+                Vynew_VS = vy_not_update;
+              end
+            1:begin
+                Vxnew_VS  = UM_Vxin;
+                Vynew_VS  = UM_Vyin;
+              end
+        endcase
+    end
+    
+    if((coll_detect1==0)&&(coll_detect2 ==0))
+    begin
+        output_check_reg=1'b1;
+        case(vs_ip_sel)
+            0:begin
+                vx1_WT = vx_not_update;
+                vy1_WT = vy_not_update;
+              end
+            1:begin
+                vx1_WT  = UM_Vxin;
+                vy1_WT  = UM_Vyin;
+              end
+        endcase
+        #10;
+        output_check_reg=1'b0;
+        ig_done_reg=1'b1;
+        #10;
+        ig_done_reg=1'b0;
+    end
+end
+   
+
+always @(posedge VS_out_rdy)
+begin
+    input_UM = 1'b1;
+    Vxnew_UM = VS_Vxout;
+    Vynew_UM = VS_Vyout;
+    vs_ip_sel= 1'b1;
+    #10;                     //Delay necessary to copy the outputs before deactivating the module
+    input_VS = 1'b0;        //To turn the module of once output is ready(simillarly with the rest);
+end
+
+endmodule
